d64060c50a2c4cfb86ac2982dbd681bfee1b0d38
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2018-07-05 11:48:07 +0200

    whoops, new bestset.py module was missing from git

---
+++ asynciojobs/bestset.py
@@ -0,0 +1,26 @@
+"""
+Schedulers and jobs requirements are essentially sets of jobs,
+and from a semantic point of view, order does not matter.
+
+However for debugging/cosmetic reasons,
+keeping track of creation order can be convenient.
+
+So using ``OrderedSet`` looks like a good idea;
+but it turns out that on some distros like fedora,
+installing ``OrderedSet`` can be a pain, as it involves recompiling C code,
+which in turn pulls in a great deal of dependencies.
+
+For this reason, we use ``OrderedSet`` only if available,
+and resort to regular sets otherwise. On macos or ubuntu, fortunately, this can be simply achieved with::
+
+    pip3 install orderedset
+
+or alternatively with::
+
+    pip3 install asynciojobs[ordered]
+"""
+
+try:
+    from orderedset import OrderedSet as BestSet
+except ModuleNotFoundError:
+    BestSet = set
